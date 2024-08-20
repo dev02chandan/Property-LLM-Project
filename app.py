529e94f351df5b19341c0cbf219a91784e7913d0
dev02chandan <dev02chandan@gmail.com>
2024-08-20 12:43:06 +0530

    App redesigned (Places API not included)

--- app.py
+++ app.py
@@ -1,132 +1,124 @@
 import streamlit as st
-import pandas as pd
-import os
-import requests
 import google.generativeai as genai
+import os
+import numpy as np
+import pandas as pd
+import textwrap
 
-# Customizing the Streamlit app
-st.markdown(
-    """
-    <style>
-    .main {
-        background-color: #ffffff;
-    }
-    .title {
-        color: #00aeef;
-        font-family: Verdana;
-    }
-    .header {
-        text-align: center;
-        color: #ca9630;
-        font-family: Verdana;
-        padding-bottom: 50px;
-    }
-    .text-input {
-        color: #ca9630;
-        font-family: Verdana;
-    }
-    .css-1aumxhk {
-        text-align: center;
-    }
-    .css-1aumxhk img {
-        width: 400px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# App title and configuration
+st.set_page_config(page_title="Grand Welcome Chatbot")
 
-# Setup Gemini API
-api_key_gemini = os.getenv('GEMINI_API_KEY')
-if api_key_gemini is None:
-    st.error("Gemini API key is not set in environment variables. Please set it before proceeding.")
-    st.stop()
+# Configure Gemini API
+if 'GEMINI_API_KEY' in st.secrets:
+    gemini_api = st.secrets['GEMINI_API_KEY']
+else:
+    gemini_api = st.text_input('Enter Gemini API token:', type='password', key='api_input')
+    if gemini_api and gemini_api.startswith('r8_') and len(gemini_api) == 40:
+        st.secrets['GEMINI_API_KEY'] = gemini_api
+        st.experimental_rerun()
 
-genai.configure(api_key=api_key_gemini)
-model = genai.GenerativeModel('gemini-pro')
+if 'GEMINI_API_KEY' in st.secrets:
+    genai.configure(api_key=st.secrets['GEMINI_API_KEY'])
+    hide_sidebar = True
+else:
+    st.sidebar.write("Please enter your API key")
+    hide_sidebar = False
 
-# Setup Google Places API
-api_key_places = os.getenv('PLACES_API_KEY')
-places_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+# Hide sidebar if API key is set
+if hide_sidebar:
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"] {
+            display: none;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-# Function to fetch nearby places from Google Places API
-def fetch_nearby_places(lat, lon, place_type='restaurant'):
-    params = {
-        'location': f'{lat},{lon}',
-        'radius': 5000,  # Set to maximum radius
-        'type': place_type,
-        'key': api_key_places
-    }
-    response = requests.get(places_url, params=params)
-    results = response.json().get('results', [])
-    places = [(place['name'], place['vicinity']) for place in results]
-    return places
+# Add logo
+logo_path = "images/logo.png"
+if os.path.exists(logo_path):
+    st.image(logo_path, width=300)
 
-# Load property data
-@st.cache_data
-def load_property_data():
-    file_path = 'data_v2.xlsx'  # Adjust the file path if necessary
-    df = pd.read_excel(file_path)
-    return df
+# Dropdown to choose the property
+property_files = {
+    "Bearfoot Landing": "data/embeddings/Bearfoot_Landing_with_embeddings.feather",
+    "Piece of Paradise": "data/embeddings/Piece_of_Paradise_with_embeddings.feather",
+    "Ray's Chalet": "data/embeddings/Rays_Chalet_with_embeddings.feather",
+    "Secluded in Paradise": "data/embeddings/Secluded_in_Paradise_with_embeddings.feather",
+    "Sliver of Paradise": "data/embeddings/Sliver_of_Paradise_with_embeddings.feather",
+    "The Real McCoy": "data/embeddings/The_Real_McCoy_with_embeddings.feather",
+    "Treetop Paradise": "data/embeddings/Treetop_Paradise_with_embeddings.feather",
+    "Woodland Wonder": "data/embeddings/Woodland_Wonder_with_embeddings.feather",
+}
 
-properties_info = load_property_data()
+# Track the previously selected property to detect changes
+if 'prev_property_name' not in st.session_state:
+    st.session_state.prev_property_name = None
 
-# Function to generate responses using Gemini API
-def generate_response(query, context):
-    prompt = f'''
+property_name = st.selectbox("Choose the property:", list(property_files.keys()), key='selected_property')
+
+# Clear chat history if the property changes
+if st.session_state.prev_property_name != property_name:
+    st.session_state.prev_property_name = property_name
+    st.session_state.messages = [{"role": "assistant", "content": f"A `Grand Welcome` to {property_name}! How can I assist you today?"}]
+
+# Load the DataFrame with precomputed embeddings for the selected property
+df = pd.read_feather(property_files[property_name])
+
+# Function to find the best passages
+def find_best_passages(query, dataframe, top_n=7):
+    query_embedding = genai.embed_content(model='models/text-embedding-004', content=query)["embedding"]
+    dot_products = np.dot(np.stack(dataframe['Embeddings']), query_embedding)
+    top_indices = np.argsort(dot_products)[-top_n:][::-1]
+    return dataframe.iloc[top_indices]['Text'].tolist()
+
+# Function to make prompt
+def make_prompt(query, relevant_passages):
+    escaped_passages = [passage.replace("'", "").replace('"', "").replace("\n", " ") for passage in relevant_passages]
+    joined_passages = "\n\n".join(f"PASSAGE {i+1}: {passage}" for i, passage in enumerate(escaped_passages))
+    prompt = textwrap.dedent(f'''
                 You are an advanced language model designed to assist with inquiries about rental properties. Your responses should be formal, detailed, and conversational, resembling how a kind and helpful person would converse. Use the provided information to answer any questions as accurately as possible. The information is given in triple backticks. 
-                If you think that the question is about nearby places, you can output: "PLACES_API_CALL,[place_type]", where place_type resembles what the person is asking about. 
                 If you are asked about Check in and check out, provide information about early check in and late check out as well which is subject to availiblity and extra fee. 
                 If you don't have the answer, Please say: "Unfortunately I am not aware how to answer that question. Can you try framing it in a different way?"
                 ```
-                {context}
+                {joined_passages}
                 ```
 
                 Question: {query}
-                '''
-    try:
-        response = model.generate_content(prompt)
-        return response.text
-    except ValueError as e:
-        if "candidate.safety_ratings" in str(e):
-            st.error("There was an error, please refresh and try again.")
-            return None
-        else:
-            raise
+                ''')
+    return prompt
 
-def main():
-    st.image('images/logo.png', use_column_width='auto')
-    st.markdown("<h1 class='header'>Property Inquiry Chatbot</h1>", unsafe_allow_html=True)
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-    # Dropdown to select the property
-    property_name = st.selectbox("Select the property you're staying at:", options=properties_info['Unit name'].unique(), format_func=lambda x: f"{x}", key='property_name')
-    property_info = properties_info[properties_info['Unit name'] == property_name].iloc[0]
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": f"Welcome to {property_name} Chatbot! How can I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-    # Prepare context
-    context = property_info['context']
-    
-    # Get user input
-    user_question = st.text_input("What would you like to know?", key='user_question')
+# Function for generating Gemini response
+def generate_gemini_response(query):
+    relevant_passages = find_best_passages(query, df)
+    prompt = make_prompt(query, relevant_passages)
+    response = genai.GenerativeModel('models/gemini-1.5-flash-latest').generate_content(prompt)
+    return response.text
 
-    if user_question:
-        response = generate_response(user_question, context)
+# User-provided prompt
+if prompt := st.chat_input(disabled=not gemini_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-        if response is not None:
-            # Check if the response is instructing to call Places API
-            if response.startswith("PLACES_API_CALL"):
-                place_type = response.split(',')[1].strip().lower()
-                if place_type == 'gardens':
-                    place_type = 'park'  # Adjust for missing types in API
-                places = fetch_nearby_places(property_info['latitude'], property_info['longitude'], place_type)
-                if places:
-                    places_formatted = '<br>'.join([f"{idx + 1}. {place[0]} ({place[1]})" for idx, place in enumerate(places[:10])])
-                    reply_with_places = f"Here are some nearby {place_type} you might find interesting:<br>{places_formatted}"
-                    st.markdown(reply_with_places, unsafe_allow_html=True)
-                else:
-                    st.write(f"No nearby {place_type} found.")
-            else:
-                # Display the original response if not a Places API call
-                st.write("Answer:", response)
-
-if __name__ == "__main__":
-    main()
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_gemini_response(prompt)
+            placeholder = st.empty()
+            placeholder.markdown(response)
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
